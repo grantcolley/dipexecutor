ff94662fcacde957cd3c12c33c794b982e956468
grantcolley <grantcolley@yahoo.com>
2017-07-28 20:35:39 +0100

    Initial add for web api project.
    Initial add for web api project.

---
+++ DipDistributeWebApi/Controllers/DistributorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace DipDistributeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DistributorController : Controller
+    {
+        [HttpGet]
+        public HttpResponseMessage GetDependency(string path)
+        {
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            return result;
+        }
+
+        [HttpPost]
+        public void Log([FromBody]string value)
+        {
+        }
+    }
+}
