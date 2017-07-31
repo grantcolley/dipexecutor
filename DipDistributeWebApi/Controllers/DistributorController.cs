10a744651d4cbb015905f2520180af18206ad35e
grantcolley <grantcolley@yahoo.com>
2017-07-31 21:38:02 +0100

    Specify Route for actions and implement GetDependency
    Specify Route for actions and implement GetDependency

--- DipDistributeWebApi/Controllers/DistributorController.cs
+++ DipDistributeWebApi/Controllers/DistributorController.cs
@@ -1,27 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.IO;
-using System.Net.Http;
-using System.Net;
-using System.Net.Http.Headers;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DipDistribute;
 
 namespace DipDistributeWebApi.Controllers
 {
     [Route("api/[controller]")]
     public class DistributorController : Controller
     {
+        // GET api/distributor/getdependency
         [HttpGet]
-        public HttpResponseMessage GetDependency(string path)
+        [Route("GetDependency")]
+        public async Task<IActionResult> GetDependency(string file)
         {
-            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
-            var stream = new FileStream(path, FileMode.Open, FileAccess.Read);
-            result.Content = new StreamContent(stream);
-            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-            return result;
+            if (string.IsNullOrEmpty(file))
+            {
+                return null;
+            }
+
+            var stream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            var response = File(stream, "application/octet-stream");
+            return response;
         }
 
-        [HttpPost]
+        // PUT api/distributor/run
+        [HttpPut]
+        [Route("Run")]
+        public async void Run([FromBody]Step step)
+        {
+            var distributor = new Distributor();
+            await distributor.RunAsync(step);
+        }
+
+        // PUT api/distributor/log
+        [HttpPut]
+        [Route("Log")]
         public void Log([FromBody]string value)
         {
+            // TODO: write to log...
         }
     }
 }
