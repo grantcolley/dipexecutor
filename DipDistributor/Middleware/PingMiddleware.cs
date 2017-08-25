32bcebe2fabcf51454f40873b775a36c7c53d956
grantcolley <grantcolley@yahoo.com>
2017-08-25 11:48:48 +0100

    Add PingMiddleware
    Add PingMiddleware

---
+++ DipDistributor/Middleware/PingMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace DipDistributor.Middleware
+{
+    public class PingMiddleware
+    {
+        public PingMiddleware(RequestDelegate next)
+        {
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await context.Response.WriteAsync($"{Environment.MachineName} Alive");
+        }
+    }
+}
