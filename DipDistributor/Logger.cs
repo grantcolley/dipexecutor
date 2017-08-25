b7fb5b5f1c763711c7a2fc11f32ada47b42ce830
grantcolley <grantcolley@yahoo.com>
2017-08-25 12:50:02 +0100

    Add temporary static logger
    Add temporary static logger

---
+++ DipDistributor/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace DipDistributor
+{
+    public static class Logger
+    {
+        private static object locker = new object();
+
+        public static async Task LogAsync(string message)
+        {
+            // TODO: get from config...
+            string path = @"C:\GitHub\dipdistributor\DipDistributor.txt";
+
+            // TODO: clean this stuff up. Testing only.
+            lock (locker)
+            {
+                if (!File.Exists(path))
+                {
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine(message);
+                    }
+                }
+
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+    }
+}
