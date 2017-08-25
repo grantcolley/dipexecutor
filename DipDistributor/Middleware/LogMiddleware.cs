f8ea85625f8852a67a0eed97fa2da29adc67a218
grantcolley <grantcolley@yahoo.com>
2017-08-25 11:47:30 +0100

    Add LogMiddleware
    Add LogMiddleware

---
+++ DipDistributor/Middleware/LogMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DipDistributor.Middleware
+{
+    public class LogMiddleware
+    {
+        public LogMiddleware(RequestDelegate next)
+        {
+        }
+
+        private object locker = new object();
+
+        public async Task Invoke(HttpContext context)
+        {
+            string body;
+            var stream = context.Request.Body;
+            using (var reader = new StreamReader(stream))
+            {
+                body = await reader.ReadToEndAsync();
+            }
+
+            // TODO: get from config...
+            string path = @"C:\GitHub\dipdistributor\DipDistributor.txt";
+
+            // TODO: clean this stuff up. Testing only.
+            lock (locker)
+            {
+                if (!File.Exists(path))
+                {
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine(body);
+                    }
+
+                    return;
+                }
+
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(body);
+                }
+            }
+        }
+    }
+}
