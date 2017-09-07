0eaf929fda5c325dcb6bba11bfecdfcd9fedf683
grantcolley <grantcolley@yahoo.com>
2017-09-07 13:54:20 +0100

    Create HttpClientFactory.
    Create HttpClientFactory.

---
+++ src/DipDistributor/DistributorHttpClientFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DipDistributor
+{
+    internal class DistributorHttpClientFactory : HttpClientFactory
+    {
+        internal override HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
---
+++ src/DipDistributor/HttpClientFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace DipDistributor
+{
+    internal abstract class HttpClientFactory
+    {
+        internal abstract HttpClient GetHttpClient();
+    }
+}
