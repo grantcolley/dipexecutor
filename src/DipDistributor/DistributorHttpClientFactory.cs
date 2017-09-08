3aded3892520b8393c49287e99359753996b9f68
grantcolley <grantcolley@yahoo.com>
2017-09-08 09:13:48 +0100

    Create single HttpClient per host.
    Create single HttpClient per host.

--- src/DipDistributor/DistributorHttpClientFactory.cs
+++ src/DipDistributor/DistributorHttpClientFactory.cs
@@ -1,16 +1,36 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace DipDistributor
 {
     internal class DistributorHttpClientFactory : HttpClientFactory
     {
+        private static HttpClient httpClient;
+        private static object httpClientLock = new object();
+
         internal override HttpClient GetHttpClient()
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            return client;
+            if (httpClient == null)
+            {
+                lock (httpClientLock)
+                {
+                    if (httpClient == null)
+                    {
+                        httpClient = new HttpClient();
+                        httpClient.DefaultRequestHeaders.Accept.Clear();
+                        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    }
+                }
+            }
+
+            //https://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/
+            //http://byterot.blogspot.co.uk/2016/07/singleton-httpclient-dns.html?m=1
+            //var sp = ServicePointManager.FindServicePoint(new Uri("URI HERE....."));
+            //sp.ConnectionLeaseTimeout = 60 * 1000;
+
+            return httpClient;
         }
     }
 }
