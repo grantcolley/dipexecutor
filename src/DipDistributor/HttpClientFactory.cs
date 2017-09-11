ab8aadcf59844d388723f2c8d96957f602922742
grantcolley <grantcolley@yahoo.com>
2017-09-11 21:00:50 +0100

    HttpClient factory.
    HttpClient factory.

--- src/DipDistributor/HttpClientFactory.cs
+++ src/DipDistributor/HttpClientFactory.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("DipDistributor.Test")]
 namespace DipDistributor
 {
     internal abstract class HttpClientFactory
