478c6f7dfcfc55908105a72dc4658a479566f842
grantcolley <grantcolley@yahoo.com>
2017-10-12 20:49:04 +0100

    Add webhost extension
    Add webhost extension

---
+++ src/DipExecutor/Service/WebHostExtensions.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IExecutor.cs" company="Development In Progress Ltd">
+//     Copyright © 2017. All rights reserved.
+// </copyright>
+// <author>Grant Colley</author>
+//-----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+
+namespace DipExecutor.Service
+{
+    /// <summary>
+    /// DipExecutor extensions for <see cref="WebHost"/>
+    /// </summary>
+    public static class WebHostExtensions
+    {
+        public static IWebHostBuilder UseExecutorStartup(this IWebHostBuilder webHost)
+        {
+            return webHost.UseStartup<Startup>();
+        }
+    }
+}
