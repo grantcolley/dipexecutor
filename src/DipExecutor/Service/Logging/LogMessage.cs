af29634b8a8a38247562c4b1ab51864aae2ae79d
grantcolley <grantcolley@yahoo.com>
2017-10-13 09:06:50 +0100

    Add logging classes
    Add logging classes

---
+++ src/DipExecutor/Service/Logging/LogMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DipExecutor.Service.Logging
+{
+    public struct LogMessage
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public string Message { get; set; }
+    }
+}
